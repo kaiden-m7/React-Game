47bfc1018fba94b513ee76ff5ea3639c067346e0
Kaiden Munsey <kaidenmunsey7@gmail.com>
2020-08-16 19:39:24 -0400

    adding characters array

---
+++ src/components/images.js
@@ -0,0 +1,114 @@
+// eslint-disable-next-line no-unused-expressions
+[
+    {
+        "id": 1,
+        "name": "Patrick Start",
+        "clicked": false,
+        "image": "https://m.media-amazon.com/images/I/41isOEwa4PL._AC_SS350_.jpg"
+    },
+
+    {
+        "id": 2,
+        "name": "Squidward Tentacles",
+        "clicked": false,
+        "image": "https://pbs.twimg.com/profile_images/1256483847556018176/pAoS3__i_400x400.jpg"
+    },
+
+    {
+        "id": 3,
+        "name": "SpongeBob SqaurePants",
+        "clicked": false,
+        "image": "https://lh6.googleusercontent.com/-yUJRGfef2LM/TWkwluSS0NI/AAAAAAAABDs/UBR64fvdYpM/s1600/2.jpg"
+    },
+
+    {
+        "id": 4,
+        "name": "Mr Krabs",
+        "clicked": false,
+        "image": "https://i.pinimg.com/originals/07/4b/d7/074bd7dfc19abf84f5cd6cb70619c703.jpg"
+    },
+
+    {
+        "id": 5,
+        "name": "Sandy Cheeks",
+        "clicked": false,
+        "image": "https://miro.medium.com/max/470/1*UybaRS4lAhgxVFDYuC_K0g.jpeg"
+    },
+
+    {
+        "id": 6,
+        "name": "Plankton",
+        "clicked": false,
+        "image": "https://i.pinimg.com/originals/24/e2/3e/24e23e643702c08f2ca42f8a79121e34.png"
+    },
+
+    {
+        "id": 7,
+        "name": "Gary",
+        "clicked": false,
+        "image": "https://assets.entrepreneur.com/content/3x2/2000/20180521195827-gary-spongebob.jpeg"
+    },
+
+    {
+        "id": 8,
+        "name": "Pearl Krabs",
+        "clicked": false,
+        "image": "https://topicimages.mrowl.com/large/gracie/spongebobsquar/thecharacters/pearlkrabs_1.jpg"
+    },
+
+    {
+        "id": 9,
+        "name": "Ms Puff",
+        "clicked": false,
+        "image": "https://i.imgur.com/SFWR69c.png"
+    },
+
+    {
+        "id": 10,
+        "name": "Karen",
+        "clicked": false,
+        "image": "https://i.pinimg.com/originals/71/27/fb/7127fb1edd063a1b9f6486fe4f919d61.jpg"
+    },
+
+    {
+        "id": 11,
+        "name": "Larry the Lobster",
+        "clicked": false,
+        "image": "https://pbs.twimg.com/profile_images/628591022621245440/QqTGKcwT_400x400.jpg"
+    },
+
+    {
+        "id": 12,
+        "name": "King Neptune",
+        "clicked": false,
+        "image": "https://i.pinimg.com/originals/85/57/9b/85579b8346dc5e2fb6b11490ae86177e.jpg"
+    },
+
+    {
+        "id": 13,
+        "name": "Flying Dutchman",
+        "clicked": false,
+        "image": "https://i.pinimg.com/originals/85/57/9b/85579b8346dc5e2fb6b11490ae86177e.jpg"
+    },
+
+    {
+        "id": 14,
+        "name": "Crabby Patty",
+        "clicked": false,
+        "image": "https://aseatenontv.files.wordpress.com/2013/03/krabby-patty-2.jpg"
+    },
+
+    {
+        "id": 15,
+        "name": "Parents",
+        "clicked": false,
+        "image": "https://i.pinimg.com/474x/7d/e0/33/7de03306e61ed5b71bba0fb03d9de6d8.jpg"
+    },
+
+    {
+        "id": 16,
+        "name": "Dirty Bubble",
+        "clicked": false,
+        "image": "https://i.redd.it/i6x576yxhkc41.jpg"
+    }
+]
