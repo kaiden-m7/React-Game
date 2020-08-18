24fd4d71f9a2463d9c43edf1b1a63d5595800b77
Kaiden Munsey <kaidenmunsey7@gmail.com>
2020-08-17 22:11:42 -0400

    adding new folder structure and server.js

---
+++ client/build/precache-manifest.34459abd82d7466082f71ea8eff74c2f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "933c0461b7080b5a830c613003443cf3",
+    "url": "/React-Game.git/index.html"
+  },
+  {
+    "revision": "264b8c83e8a529b97829",
+    "url": "/React-Game.git/static/css/main.247757ce.chunk.css"
+  },
+  {
+    "revision": "6abe279e2d971adc62f2",
+    "url": "/React-Game.git/static/js/2.4cd81a96.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Game.git/static/js/2.4cd81a96.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "264b8c83e8a529b97829",
+    "url": "/React-Game.git/static/js/main.de9b1925.chunk.js"
+  },
+  {
+    "revision": "43d51d78a406b2d537c6",
+    "url": "/React-Game.git/static/js/runtime-main.241e18b4.js"
+  }
+]);
